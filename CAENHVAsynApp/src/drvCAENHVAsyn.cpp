78ccc777f18f81d5431c0a37a2b114dcf76dbe89
Jeremy Mock <jeremy.a.mock@gmail.com>
2024-10-22 10:44:45 -0700

    Update make_pair function call for rhel7 compiler

--- CAENHVAsynApp/src/drvCAENHVAsyn.cpp
+++ CAENHVAsynApp/src/drvCAENHVAsyn.cpp
@@ -40,7 +40,7 @@ void CAENHVAsyn::createParamFloat(T p, std::map<int, T>& list)
     int index;
     createParam(paramName.c_str(), asynParamFloat64, &index);
 
-    list.insert( std::make_pair<int, T>(index, p) );
+    list.insert( std::make_pair(index, p) );
 
     if (!epicsPrefix.empty())
     {
@@ -87,7 +87,7 @@ void CAENHVAsyn::createParamFloat(SystemPropertyFloat p, std::map<int, SystemPro
     int index;
     createParam(paramName.c_str(), asynParamFloat64, &index);
 
-    list.insert( std::make_pair<int, SystemPropertyFloat>(index, p) );
+    list.insert( std::make_pair(index, p) );
 
     if (!epicsPrefix.empty())
     {
@@ -134,7 +134,7 @@ void CAENHVAsyn::createParamBinary(T p, std::map<int, T>& list)
     int index;
     createParam(paramName.c_str(), asynParamUInt32Digital, &index);
 
-    list.insert( std::make_pair<int, T>(index, p) );
+    list.insert( std::make_pair(index, p) );
 
     if (!epicsPrefix.empty())
     {
@@ -175,7 +175,7 @@ void CAENHVAsyn::createParamMBinary(T p, std::map<int, T>& list, const statusRec
     int index;
     createParam(paramName.c_str(), asynParamUInt32Digital, &index);
 
-    list.insert( std::make_pair<int, T>(index, p) );
+    list.insert( std::make_pair(index, p) );
 
     if (!epicsPrefix.empty())
     {
@@ -233,7 +233,7 @@ void CAENHVAsyn::createParamInteger(T p, std::map<int, T>& list)
     int index;
     createParam(paramName.c_str(), asynParamInt32, &index);
 
-    list.insert( std::make_pair<int, T>(index, p) );
+    list.insert( std::make_pair(index, p) );
 
     if (!epicsPrefix.empty())
     {
@@ -272,7 +272,7 @@ void CAENHVAsyn::createParamString(T p, std::map<int, T>& list)
     int index;
     createParam(paramName.c_str(), asynParamOctet, &index);
 
-    list.insert( std::make_pair<int, T>(index, p) );
+    list.insert( std::make_pair(index, p) );
 
     if (!epicsPrefix.empty())
     {
